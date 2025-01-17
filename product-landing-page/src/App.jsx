b38e4235724ef26e68b404231b6716208ef1dd05
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-01-17 19:36:08 +0530

    Minor Changes Made

--- product-landing-page/src/App.jsx
+++ product-landing-page/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <h1>Hello World is the Target of the Program...</h1>
       <h1> Thank You ...</h1>
        <h3>Only Doing the Daily streaks of the Github...</h3>
+       <h1>Only Due to the Completion of the Streak ...</h1>
   </div>;
 }
 
