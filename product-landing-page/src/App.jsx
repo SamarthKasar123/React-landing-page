1f5607c75cb7db50ff0a836f7713ff52de948f5b
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-01-22 21:21:18 +0530

    Minor CHanges made in Fronntend COde

--- product-landing-page/src/App.jsx
+++ product-landing-page/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
       <h1> Thank You ...</h1>
        <h3>Only Doing the Daily streaks of the Github...</h3>
        <h1>Only Due to the Completion of the Streak ...</h1>
+        <h1>Day 29 STreak completion</h1>
   </div>;
 }
 
